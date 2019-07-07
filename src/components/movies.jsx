0b44b84e93eefc5e6f6ce28bda2dac72514af652
Andrew Ormond <drewthedev9@gmail.com>
2019-07-07 16:27:57 -0400

    added a onclick function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,12 @@ class Movies extends Component {
     state = {
      movies: getMovies()
       };
+
+    //   arrow function will allow for the acces to the object and function above
+      handleDelete=(movie)=>{
+          console.log(movie);
+
+      }
     render() { 
         return (
         
@@ -14,16 +20,18 @@ class Movies extends Component {
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
-                    <th>Genre</th>
+                    <th>Rate</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
             {this.state.movies.map(movie => 
-            <tr>
+            <tr key={movie._id} >
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><button onClick={()=>this.handleDelete(movie)}className = "btn btn-danger btn-sm">Delete</button></td>
             </tr>)}
                 
             </tbody>
