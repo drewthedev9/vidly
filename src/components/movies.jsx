952398de14f82b5b5f5b7ee2fa5e8fed88c22652
Andrew Ormond <drewthedev9@gmail.com>
2019-07-07 15:08:31 -0400

    basc react set up

---
+++ src/components/movies.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h2>Movies Compnent</h2>
+         );
+    }
+}
+ 
+export default Movies;
